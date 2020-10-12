fa099880cd974661f2752d110516686e3167574b
Marko Dunovic <phoene1990x@gmail.com>
2020-10-12 19:17:18 +0200

    update
    update

---
+++ Termin15/Termin15ZadatakPoAssigment/app/src/main/java/com/example/termin14zadatak/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.termin14zadatak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+  //  private ListView lstview;
+    //Vocke vockeX=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // u okviru on create metode
+        for(final Vocke x: Vocke.vocke){  // da pravi dugmad na osnovu niza
+            Button button = new Button(this);
+            button.setText(x.getName());
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override  // mora da ide main activity.this zato sto je uvucen listener pa moram da kazem klasa this a ne metoda this
+                public void onClick(View v) {  // za svako dugme da otvara novi prozor preko listenera je vezan sa metodom
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                        intent.putExtra("id", x.getId());
+                        startActivity(intent);
+
+
+                }
+            });
+
+            LinearLayout linearLayout = findViewById(R.id.linear1); // id od xml-a
+            linearLayout.addView(button); // dugme koje je pravljeno
+        }
+
+
+        /*for(final Vocke x: Vocke.vocke){
+            vockeX = new Vocke(x.getId(), x.getName(),x.getOpis(),x.getImagefilename(),x.getRating());
+            System.out.println(vockeX);
+        }*/
+
+
+    }
+
+
+
+    /*public void bwutton(View view){
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+    }*/
+}
