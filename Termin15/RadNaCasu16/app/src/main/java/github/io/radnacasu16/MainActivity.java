5aa34178f0e938ca8906852f61678909039b1579
Marko Dunovic <phoene1990x@gmail.com>
2020-10-12 19:46:51 +0200

    update
    save point

--- Termin15/RadNaCasu16/app/src/main/java/github/io/radnacasu16/MainActivity.java
+++ Termin15/RadNaCasu16/app/src/main/java/github/io/radnacasu16/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relativni_raspored); // relativni raspored
+        setContentView(R.layout.activity_main); // relativni raspored
     }
 
 
