27f4b84b5d3ba2bd53c7c937c6cf90675227a889
Marko Dunovic <phoene1990x@gmail.com>
2020-11-16 18:10:05 +0100

    asd
    asd

---
+++ Sort/Example28/Termin29b/app/src/main/java/rs/aleph/termin29/GenericFileProvider.java
@@ -0,0 +1,13 @@
+package rs.aleph.termin29;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by milossimic on 10/28/17.
+ */
+
+public class GenericFileProvider extends FileProvider {
+
+    public static final String MY_PROVIDER = "rs.aleph.termin29.provider";
+
+}
